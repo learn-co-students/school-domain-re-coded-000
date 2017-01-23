362db56775fe4020ead4715f42a5e9b8f4a88383
noven temer <noven5823@gmail.com>
2017-01-23 19:15:05 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+def initialize(name)
+  @roster={}
+  @name=name
+end
+def roster
+  @roster
+end
+def add_student(name, grade)
+  roster[grade] ||= []
+  @roster[grade] << name
+end
+def grade(grade)
+  @roster[grade]
+end
+def sort
+  grad={}
+  @roster.each do |key, value|
+    grad[key]=value.sort
+  end
+  grad
+end
+end
